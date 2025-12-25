56934409b2c47e310602d1750e337967f2a7c76a
Arush_warty <parush.larty@gmail.com>
2025-12-25 17:49:05 +0530

    forgtotten commit for trimming and vm intergration

--- Song-extract-test.py
+++ Song-extract-test.py
@@ -1,1 +1,21 @@
-print("hello world" )
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+from glob import glob
+import seaborn as sns
+import librosa
+import librosa.display
+
+audio_files = glob(
+    r"C:\Users\warty\OneDrive\Desktop\Python_projects\Capstone_music_maker\Scenario 2_ AI Music Composer & Listener Insight platform\MP3-Example\**\*.mp3"
+)
+
+#y=raw song data, sr=sampling rate
+
+y,sr=librosa.load(audio_files[0])
+
+
+pd.Series(y).plot(figsize=(14,5))
+
+plt.show()
